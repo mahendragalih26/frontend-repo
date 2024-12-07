474ba875170655ad104b0f6d0a3e928019286acb
Galih Mahendra <galihmahendraw@gmail.com>
2024-12-07 13:52:22 +0700

    update

--- apis/firebase.ts
+++ apis/firebase.ts
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app"
 
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id",
+  apiKey: "AIzaSyBd0GoRNPygr2nZHSO-YPQeKSDGGeHpxk8",
+  authDomain: "frontend-repo-f7425.firebaseapp.com",
+  projectId: "frontend-repo-f7425",
+  storageBucket: "frontend-repo-f7425.firebasestorage.app",
+  messagingSenderId: "309962677419",
+  appId: "1:309962677419:web:1e1e5159a115e0129aebee",
+  measurementId: "G-GTQZG0KMRQ",
 }
 
 const app = initializeApp(firebaseConfig)
